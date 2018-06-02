1ab7872e8c99e0fb8125ea1402107b74883fafe5
marxcar <shannat@symmesdarcy.com>
2018-06-02 19:42:55 -0400

    Added compatibilty with multivariable linear regression

--- Andrew-ng Course/Week 2/main.py
+++ Andrew-ng Course/Week 2/main.py
@@ -12,6 +12,7 @@ plt.ylabel("Profit in $10,000s")
 
 x = np.c_[np.ones(df.shape[0]),df[:,0]]
 y = np.c_[df[:,1]]
+
 theta = np.array([[0.],[0.]])
 iterations = 1500
 alpha = 0.01
@@ -27,17 +28,22 @@ def mean_squared_error(t,c,b):
 	error = hypothesis(t,c) - b
 
 	return float((1.)/(2*m)*np.dot(error.T, error))
-def gradient_descent(w, xs, ys, a, frequency):
-	m = len(xs)
 
+
+
+def gradient_descent_runner(w, xs, ys, a, frequency):
+	m = len(xs)
+	temp = []
 	for f in range(frequency):
 
-
-		temp0 = sum([(hypothesis(w,xs[i]) -ys[i]) for i in range(m)])
-		temp1 = sum([xs[i,1]*(hypothesis(w,xs[i]) -ys[i]) for i in range(m)])
-
-		w[0,0] = (w[0,0] - a*(temp0)/m)
-		w[1,0] = (w[1,0] - a*(temp1)/m)
+		for t in range(len(w)):
+	
+			temp.append(sum([xs[i,t]*(hypothesis(w,xs[i]) -ys[i]) for i in range(m)]))
+		
+		for k in range(len(temp)):
+			w[k,0] = w[k,0] - a*(temp[k]/m)
+	
+		temp = []
 		
 	return w
 def line(theta, xvalues):
@@ -46,11 +52,9 @@ def line(theta, xvalues):
 
 print("Error before gradient_descent: %f" % mean_squared_error(theta,x,y))
 
-theta = gradient_descent(theta, x,y,alpha,iterations)
+theta = gradient_descent_runner(theta, x,y,alpha,iterations)
 
 print("Final Weights: " + str(theta))
 print("Final Error: %f" %(mean_squared_error(theta,x,y)))
 
 
-plt.plot(x[:,1], line(theta, x[:,1]))
-plt.show()
