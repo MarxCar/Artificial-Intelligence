Fixed logistic regression with gradient descent

Andrew-ng Course/Week 3/main.py
@@ -3,11 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from scipy.optimize import minimize
+from sklearn import linear_model
 data = np.loadtxt("ex2data1.txt", delimiter=",")
 
 
 X = np.c_[np.ones(data.shape[0]),data[:,:2]]
-y = np.c_[data[:,-1]]
+
+y = np.c_[data[:,2]]
 iterations = 1500
 alpha = 0.01
 m = len(X)
@@ -25,7 +27,6 @@ for i in range(m):
 	plt.scatter(data[i,0], data[i,1], c=color, marker=marker)
 
 
-#plt.show()
 
 
 def sigmoid(z):
@@ -34,32 +35,41 @@ def sigmoid(z):
 def hypothesis(xval,t):
 	return sigmoid(np.dot(xval,t))
 
-def costFunction(x,y, theta):
+def costFunction(theta,x,y):
 	c = hypothesis(x,theta)
-	return (np.dot(-y.T,np.log(c)) -np.dot(1-y.T, np.log(1-c)))/len(x)
+	J = (np.dot(-y.T,np.log(c)) -np.dot(1-y.T, np.log(1-c)))/len(x)
 
-def costFunctionRunner(t):
-	return costFunction(X,y,t)
+	if np.isnan(J[0]):
+		return(np.inf)
+	return(J[0])
 
-def gradientDescent(x,y,a,generations):
-	t = np.zeros((x.shape[1],1))
+def gradientDescent(t,x,y):
+	t = t.reshape(-1,1)
 	m = len(x)
-	for k in range(generations):
-		temp =  np.dot(x.T, hypothesis(x,t)-y)
-		t = t - (a/m) * temp
-	return t
+	temp =  np.dot(x.T, hypothesis(x,t)-y)
+	gradient = (1.0/m) * temp
 
-
+	return(gradient.flatten())
 
 
 theta =  np.array([[0],[0],[0]])
-print("Cost of Model Before Hand: " + str(costFunction(X,y,theta)[0]))
+print("Cost of Model Before Hand: " + str(costFunction(theta,X,y)[0]))
 
 
 #minimize cost function using SLSQP method instead of gradient descent
-res = minimize(costFunctionRunner, theta, method="SLSQP")
+res = minimize(costFunction, theta, args=(X,y), method="BFGS")
 new_thetas = res.x
+
+
 print(new_thetas)
 print("Cost of Model After SLSQP: " + str(res.fun))
 
-print(gradientDescent(X,y,0.001, 10000))
+#gradient descent
+res2 = minimize(costFunction, theta, args=(X,y), method=None, jac=gradientDescent, options={'maxiter':400})
+
+print("Cost of Model After Gradient Descent:  " + str(res2.fun))
+
+
+
+
+